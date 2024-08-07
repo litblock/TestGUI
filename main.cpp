d3b8468d2eb10106d140a7d89da7c8a689d80a75
litblock <jonathanpeng2017@gmail.com>
2024-08-06 23:29:02 -0400

    fixing project structure

--- main.cpp
+++ main.cpp
@@ -1,3 +1,40 @@
 #include "imgui.h"
 #include "imgui_impl_sdl2.h"
 #include "imgui_impl_opengl3.h"
+
+```
+https://github.com/ocornut/imgui/wiki/Getting-Started#example-if-you-are-using-sdl2--openglwebgl
+im learning how to setup the file structure
+```
+
+// Setup Dear ImGui context
+IMGUI_CHECKVERSION();
+ImGui::CreateContext();
+ImGuiIO& io = ImGui::GetIO();
+io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;         // IF using Docking Branch
+
+// Setup Platform/Renderer backends
+ImGui_ImplSDL2_InitForOpenGL(window, YOUR_SDL_GL_CONTEXT);
+ImGui_ImplOpenGL3_Init();
+
+// (Where your code calls SDL_PollEvent())
+ImGui_ImplSDL2_ProcessEvent(&event); // Forward your event to backend
+
+// (After event loop)
+// Start the Dear ImGui frame
+ImGui_ImplOpenGL3_NewFrame();
+ImGui_ImplSDL2_NewFrame();
+ImGui::NewFrame();
+ImGui::ShowDemoWindow(); // Show demo window! :)
+
+// Rendering
+// (Your code clears your framebuffer, renders your other stuff etc.)
+ImGui::Render();
+ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+// (Your code calls SDL_GL_SwapWindow() etc.)
+
+ImGui_ImplOpenGL3_Shutdown();
+ImGui_ImplSDL2_Shutdown();
+ImGui::DestroyContext();
