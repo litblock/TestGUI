e8fc99265fb80ddd04d5f3fe3a6481e212f419de
litblock <jonathanpeng2017@gmail.com>
2024-08-07 22:00:38 -0400

    FINALLY WORKS LMAO

--- Test/test.cpp
+++ Test/test.cpp
@@ -1,78 +1,68 @@
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <stdlib.h>
-#include <stdio.h>
 #include <iostream>
 
-using namespace std;
-
-static void error_callback(int error, const char* description)
+void error_callback(int error, const char* description)
 {
-    fputs(description, stderr);
+    (void)error; 
+    std::cerr << "Error: " << description << std::endl;
 }
-static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
+    (void)scancode; 
+    (void)mods;     
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GL_TRUE);
+        glfwSetWindowShouldClose(window, true);
 }
-int main(void)
+
+int main()
 {
-    GLFWwindow* window;
+
     glfwSetErrorCallback(error_callback);
+
     if (!glfwInit())
-        exit(EXIT_FAILURE);
+    {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
 
-    // cout << "default shader lang: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << endl;
 
-    // select opengl version
-    // int major, minor, rev;
-    // glfwGetVersion(&major, &minor, &rev);
-    // cout << "glfw major.minor " << major << "." << minor << "." << rev << endl;
-    
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-
-
-    window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
-    if (!window)
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
     {
+        std::cerr << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
-        exit(EXIT_FAILURE);
+        return -1;
     }
+
     glfwMakeContextCurrent(window);
 
-    cout << "OpenGL shader language version: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << endl;
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cerr << "Failed to initialize GLAD" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
 
+    glViewport(0, 0, 800, 600);
 
     glfwSetKeyCallback(window, key_callback);
     while (!glfwWindowShouldClose(window))
     {
-        float ratio;
-        int width, height;
-        glfwGetFramebufferSize(window, &width, &height);
-        ratio = width / (float) height;
-        glViewport(0, 0, width, height);
         glClear(GL_COLOR_BUFFER_BIT);
-        glMatrixMode(GL_PROJECTION);
-        glLoadIdentity();
-        glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
-        glMatrixMode(GL_MODELVIEW);
-        glLoadIdentity();
-        glRotatef((float) glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
-        glBegin(GL_TRIANGLES);
-        glColor3f(1.f, 0.f, 0.f);
-        glVertex3f(-0.6f, -0.4f, 0.f);
-        glColor3f(0.f, 1.f, 0.f);
-        glVertex3f(0.6f, -0.4f, 0.f);
-        glColor3f(0.f, 0.f, 1.f);
-        glVertex3f(0.f, 0.6f, 0.f);
-        glEnd();
+
+
         glfwSwapBuffers(window);
+
         glfwPollEvents();
     }
+
     glfwDestroyWindow(window);
     glfwTerminate();
-    exit(EXIT_SUCCESS);
+    return 0;
 }
